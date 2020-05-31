c9ff8b33afe35bbf8beae04c4e731a29167894f9
Alexej <alexejdubow@gmail.com>
2020-05-31 17:45:42 +0300

    Updates

---
+++ precache-manifest.858cff61f5f193f24248a4910fd09acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4ea15cb14c1d673924ca88d9416408",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "04a05c689a4f1f20eb8d",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.19b0af54.chunk.css"
+  },
+  {
+    "revision": "ea566bd0cde04647d530",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.ab277f80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.ab277f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a05c689a4f1f20eb8d",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.69ad87a6.chunk.js"
+  },
+  {
+    "revision": "412989084c224794c988",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.77bb1b5a.js"
+  }
+]);
